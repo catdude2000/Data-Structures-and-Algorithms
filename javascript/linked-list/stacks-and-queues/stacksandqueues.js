f54fb78d6191ff432cc68c31acc3b1411d34c57b
Michael Pace <michael.g.pace@gmail.com>
2023-08-01 22:41:19 -0500

    stacksandqueues js done

--- javascript/linked-list/stacks-and-queues/stacksandqueues.js
+++ javascript/linked-list/stacks-and-queues/stacksandqueues.js
@@ -17,16 +17,49 @@ class Stack {
   }
   push(value) {
     let node = new Node(value);
-    node.next = top;
-    let top = node;
+    node.next = this.top;
+    this.top = node;
   }
   pop() {
-    let temp = top;
-    top = top.next;
+    let temp = this.top;
+    this.top = temp.next;
     temp.next = null;
     return temp.value; //or just temp
   }
   peek() {
-    return top.value;
+    return this.top.value;
+  }
+  isEmpty() {
+    return this.top = null;
   }
 }
+
+class Queue {
+  constructor(front = null, rear = null) {
+    this.front = front;
+    this.rear = rear;
+  }
+  enqueue(value) { //O(1) time
+
+    let node = new Node(value);
+    this.rear.next = node;
+    this.rear = node;
+    if (!this.front) {
+      this.front = node;
+    }
+  }
+  dequeue() {
+    let temp = this.front;
+    this.front = this.front.next;
+    temp.next = null;
+    return temp.value;
+  }
+  peek() {
+    return this.front.value;
+  }
+  isEmpty() {
+    return this.front = null;
+  }
+}
+
+module.exports = { Node, Stack, Queue };
