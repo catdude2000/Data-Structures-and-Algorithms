e64744cfca5f26a3b247893fd3510cb593b287dc
Michael Pace <michael.g.pace@gmail.com>
2023-08-21 20:47:43 -0500

    added can tests reqs

--- javascript/linked-list/trees/trees.test.js
+++ javascript/linked-list/trees/trees.test.js
@@ -29,3 +29,12 @@ describe('TreeNode', () => {
 
   // Add more test cases for other TreeNode-related functionality
 });
+
+
+// Can successfully instantiate an empty tree
+// Can successfully instantiate a tree with a single root node
+// For a Binary Search Tree, can successfully add a left child and right child properly to a node
+// Can successfully return a collection from a pre-order traversal
+// Can successfully return a collection from an in-order traversal
+// Can successfully return a collection from a post-order traversal
+// Returns true	false for the contains method, given an existing or non-existing node value
