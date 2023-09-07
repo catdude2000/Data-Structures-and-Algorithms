1edbeb70c017448796c2c2793270417e6daa0e18
Michael Pace <michael.g.pace@gmail.com>
2023-09-06 22:17:46 -0500

    finished wb added, readme basics all t here

--- javascript/trees/tree-fizz-buzz/README.md
+++ javascript/trees/tree-fizz-buzz/README.md
@@ -2,12 +2,18 @@
 
 ## Whiteboard Process
 
-![whiteboard image](/javascript/trees)
+![whiteboard image](/javascript/trees/tree-fizz-buzz/cc18WB.png)
 
 ## Approach & Efficiency
+
+Traverse tree and change values as neccesary.
+
+Space and time O(n)
 
 ## Solution
 
 [code](./tree-fizz-buzz.js)
 
 npm test tree-fizz-buzz
+
+Got help from ChatGPT  
--- javascript/trees/tree-fizz-buzz/tree-fizz-buzz.js
+++ javascript/trees/tree-fizz-buzz/tree-fizz-buzz.js
@@ -1,17 +1,3 @@
-// ALGORITHM breadthFirst(root)
-// // INPUT  <-- root node
-// // OUTPUT <-- front node of queue to console
-
-//   Queue breadth <-- new Queue()
-//   breadth.enqueue(root)
-
-//   while ! breadth.is_empty()
-//     node front = breadth.dequeue()
-//     OUTPUT <-- front.value
-
-//     for child in front.children
-//         breadth.enqueue(child)
-
 class KaryTreeNode {
   constructor(value, children = []) {
     this.value = value;
@@ -24,7 +10,6 @@ function fizzBuzzKaryTree(root) {
     if (node === null) {
       return null;
     }
-
     let newValue = '';
     if (node.value % 3 === 0) {
       newValue += 'Fizz';
@@ -32,29 +17,16 @@ function fizzBuzzKaryTree(root) {
     if (node.value % 5 === 0) {
       newValue += 'Buzz';
     }
-
     if (newValue === '') {
       newValue = node.value;
     }
-
     const newChildren = [];
     for (const child of node.children) {
       newChildren.push(fizzBuzz(child));
     }
-
     return new KaryTreeNode(newValue, newChildren);
   }
-
   return fizzBuzz(root);
 }
-
-// Example usage:
-// const tree = new KaryTreeNode(15, [
-//   new KaryTreeNode(9, [new KaryTreeNode(6)]),
-//   new KaryTreeNode(10),
-// ]);
-
-// const modifiedTree = fizzBuzzKaryTree(tree);
-// console.log(modifiedTree);
 
 module.exports = { fizzBuzzKaryTree, KaryTreeNode };
